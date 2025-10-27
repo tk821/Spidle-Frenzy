34bac6499b89cf7080c4e99ba96d7766b182c07d
tk821 <191364051+tk821@users.noreply.github.com>
2025-10-27 10:29:34 -0700

    Wrote instantiation and timed loop

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const game = {
-    webs: {
+    clickPower: 1,
 
+    webs: [
+        {
+            webStrands : 0,
+            modifiers : []
+        }
+    ],
+    
+    flies: {
+        counter : 0,
+        perSecond : 0,
     }
-
-
 };
 
 
-let fliesCounter = 0;
-let webStrands = 0;
-let webs = 0;
 const flies = document.getElementById("flies");
 const catchButton = document.getElementById("catch");
 
 
+function tick() {
+    const ticksPerSecond = 20;
+    game.flies.counter += game.flies.perSecond / ticksPerSecond;
+    updateUI();
+}
+
+
+function updateUI() {
+    flies.textContent = "Flies: " + Math.floor(game.flies.counter);
+}
+
+
 catchButton.addEventListener("click", () => {
-    fliesCounter++;
-    flies.textContent = "Flies: " + fliesCounter;
+    game.flies.counter += game.clickPower;
+    flies.textContent = "Flies: " + game.flies.counter;
 });
 
+setInterval(tick, 50);
